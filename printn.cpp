9ce74a6083c6d8c25530c3978534d3a8b1857123
Shubham <shubham86.y@gmail.com>
2026-07-04 23:37:39 +0530

    Initial commit

---
+++ printn.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout << "Enter number :";
+    cin >> number;
+    for(int i=1;i<=number;i++){
+        cout << i;
+    }
+    return 0;
+}
